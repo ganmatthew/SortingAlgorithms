3dcd14bcbacb17dfce581252146cdcc350df7977
ganmatthew <robloxguy6403@gmail.com>
2020-08-21 20:08:55 +0800

    added cputime

--- InsertionSort.h
+++ InsertionSort.h
@@ -5,20 +5,26 @@
 void InsertionSort(int A[], int N) {
    int ctr = 0;
    int i, k, temp;
+   //ctr += 2;
 
    for (i = 0; i < N; i++) {
+      ctr+= 1;
       temp = A[i];
       k = i;
+      ctr += 2;
 
       while (k > 0 && temp < A[k - 1]) {
+         ctr += 1;
          A[k] = A[k - 1];
          --k;
-         ctr++;
+         ctr += 2;
       }
+      ctr += 1;
 
       A[k] = temp;
-      ctr++;
+      ctr += 1;
    }
+   ctr += 1;
 
    count(ctr);
 }
--- sortDriver.c
+++ sortDriver.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define ARRAY_SIZE 10
+#define ARRAY_SIZE 1024
 #define LIST_MIN 1
 #define LIST_MAX 10
 
@@ -11,7 +11,8 @@
 int Random (int floor, int ceiling, int cycles) { 
 	int i, num;
 	for (i = 0; i < cycles; i++) {
-		num = (rand() % (ceiling - floor + 1)) + floor;
+		//num = (rand() % ceiling) + floor;
+      num = rand();
 	}
 
 	return num;
@@ -35,6 +36,14 @@ void DisplayData (int A[], int N) {
    printf("---\n");
 }
 
+void ManualInputData (int A[], int N) {
+   int i;
+   for (i = 0; i < N; i++) {
+      printf("Enter item %d for a list of size %d: ", i + 1, N);
+      scanf("%d", &A[i]);
+   }
+}
+
 void count (int counter) {
    printf("counter: %d\n", counter);
 }
@@ -48,16 +57,17 @@ void count (int counter) {
 #include "RadixSort.h"
 
 int main () {
-   srand(time(NULL));
+   srand((unsigned) time(0)); // get seed from current time
    int list[ARRAY_SIZE];
 
    GenerateData(list, ARRAY_SIZE);
+   //ManualInputData(list, ARRAY_SIZE);
    DisplayData(list, ARRAY_SIZE);
 
    //BubbleSort(list, ARRAY_SIZE);
-   //InsertionSort(list, ARRAY_SIZE);
+   InsertionSort(list, ARRAY_SIZE);
    //SelectionSort(list, ARRAY_SIZE);
-   QuickSort(list, ARRAY_SIZE);
+   //QuickSort(list, ARRAY_SIZE);
    //MergeSort(list, ARRAY_SIZE);
    //RadixSort(list, ARRAY_SIZE);
 
